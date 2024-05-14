0bfdcb5d310fbdcc780a434e95bf6417e722a86e
neerrajpanchal004 <npvishwakarma.143@gmail.com>
2024-05-14 13:17:51 +0530

    adjest responsive design

--- src/app/appLayout.jsx
+++ src/app/appLayout.jsx
@@ -6,7 +6,7 @@ const AppLayout = ({children}) => {
     <>
     <div>
         <img src='/img/mountain-background.jpg' className='w-full h-screen object-cover'/>
-        <div className='absolute top-8  lg:top-16 sm:left-48 lg:left-96 w-80 sm:w-96  '>{children}</div>
+        <div className='absolute top-8  lg:top-16 left-4 sm:left-48 lg:left-96 w-80 sm:w-96  '>{children}</div>
     </div>
     </>
   )
